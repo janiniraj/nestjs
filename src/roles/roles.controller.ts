f87a68a03dc017a56616897bf7f4bd47bd44bdd1
Niraj Jani <niraj@integriocorp.com>
2019-06-15 11:54:05 +0800

    roles

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Put, Delete,Param} from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { Role } from './roles.entity';
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+    constructor(private service: RolesService) { }
+
+    @Get('')
+    getAllRoles() {
+        return this.service.getAllRoles();
+    }
+
+    @Get(':id')
+    get(@Param() params) {
+        return this.service.getRole(params.id);
+    }
+
+    @Post()
+    create(@Body() role: Role) {
+        return this.service.createRole(role);
+    }
+
+    @Put()
+    update(@Body() role: Role) {
+        return this.service.updateRole(role);
+    }
+
+    @Delete(':id')
+    deleteRole(@Param() params) {
+        return this.service.deleteRole(params.id);
+    }
+}
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Role } from './roles.entity';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+
+    constructor(@InjectRepository(Role) private rolesRepository: Repository<Role>) 
+    { 
+
+    }
+
+    async createRole(role:Role) {
+        this.rolesRepository.save(role)
+    }
+
+    async getRoles(role: Role): Promise<Role[]> {
+        return await this.rolesRepository.find();
+    }
+
+    async getRole(_id: number): Promise<Role[]> {
+        return await this.rolesRepository.find({
+            select: ["name", "isActive"],
+            where: [{ "id": _id }]
+        });
+    }
+
+    async updateRole(role: Role) {
+        this.rolesRepository.save(role)
+    }
+
+    async deleteRole(role: Role) {
+        this.rolesRepository.delete(role);
+    }
+
+    async getAllRoles(){
+        return await this.rolesRepository.find();
+    }
+}
